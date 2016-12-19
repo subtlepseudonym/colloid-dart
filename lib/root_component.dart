438ac1e0cff97822b5284fe159b241b9dae50d2b
maiq <subtlepseudonym@gmail.com>
2016-12-19 06:54:53 -0500

    initial commit

---
+++ lib/root_component.dart
@@ -0,0 +1,10 @@
+// Copyright (c) 2016, subtlepseudonym. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/core.dart';
+
+@Component(
+    selector: 'root-app',
+    styleUrls: const ['root_component.css'],
+    templateUrl: 'root_component.html')
+class RootComponent {}
